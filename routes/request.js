7d1784de3cfc4b908228b07451bd30670c7b7eb2
savitoj <savitojsingh15@gmail.com>
2020-06-15 22:07:48 +0530

    fetch, create request api

--- routes/request.js
+++ routes/request.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var requireJwtAuth = require("../middleware/requireJwtAuth");
+
 //var like = require("./like")
 // Require controller modules.
 var requestController = require("../Controller/requestController");
 
-router.get("/:reqId", requestController.get_request);
+router.get("/:reqId", requireJwtAuth, requestController.get_request);
 router.post("/comments", requestController.comments);
-router.post("/create", requestController.create_request);
+router.post("/create", requireJwtAuth, requestController.create_request);
 router.put("/:reqId", requestController.edit_request);
 router.delete("/:reqId", requestController.delete_request);
 //router.use("/like", like);
