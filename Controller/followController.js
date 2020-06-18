2d3899e5b31e3978533bb834cd49d3a3f51f566b
Nishant2604 <nishant@DESKTOP-JP26GV8.localdomain>
2020-06-18 14:37:25 +0530

    Minor changes in followController

--- Controller/followController.js
+++ Controller/followController.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 exports.follow = async function (req, res) {
     try{
       //saving following user's data in User's following list
-      var user = await Following.findOne({ user_id: req.params.followerId })
+      var user = await Following.findOne({ user_id: req.user._id })
       var followingUserProfile = await User.findOne({ _id : req.params.followingId })
       var followingUsername = followingUserProfile.name
       var followingUserData = {
@@ -23,14 +23,14 @@ exports.follow = async function (req, res) {
  
       //saving User's data in followinguser's followers list
       var followingUser = await Followers.findOne({ user_id: req.params.followingId })
-      var userProfile = await User.findOne({ _id : req.params.followerId })
+      var userProfile = await User.findOne({ _id : req.user._id })
       var username = userProfile.name
       var userData = {
-        user_id: new mongoose.mongo.ObjectId(req.params.followerId),
+        user_id: new mongoose.mongo.ObjectId(req.user._id),
         name: username
       }
 
-      if(followingUser.connection_array.some( array => array['user_id'] == req.params.followerId )){
+      if(followingUser.connection_array.some( array => array['user_id'] == req.user._id )){
         res.send("Item already present")
       } else {
         followingUser.connection_array.push(userData)
--- routes/follow.js
+++ routes/follow.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 // Require controller modules.
 var followController = require("../Controller/followController");
+const requireJwtAuth = require("../middleware/requireJwtAuth");
 
 //Add new function by using followsController.<function's name>
 
-router.post("/:followerId/:followingId", followController.follow);
+router.post("/:followingId", requireJwtAuth, followController.follow);
 
 module.exports = router;
