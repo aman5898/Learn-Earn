7fdd32bda289fbb177c457fa1cea3ece05a52b98
Nishant2604 <nishant@DESKTOP-JP26GV8.localdomain>
2020-06-30 20:03:04 +0530

    interested users completed

--- src/components/InterestedUsers.js
+++ src/components/InterestedUsers.js
@@ -1,15 +1,28 @@
 import React from "react";
 import styles from "../styles/App.scss";
+import image from "../temp/image.jpg";
 
 function InterestedUsers(){
     return(
-        <div>
-            <div className={styles.plus_symbol}>
-                +
-            </div>
-                
+        <div className="row">
             <div className="col">
-                
+                <div className={styles.plus_symbol}>
+                    +
+                </div>
+            </div>
+            <div className="col-8">
+                <div className="row">
+                    <div className={styles.int_images}>
+                         <img src={image} alt="interested users" className={styles.intUser_pic} />
+                         <img src={image} alt="interested users" className={styles.intUser_pic && styles.pic_two} />
+                         <img src={image} alt="interested users" className={styles.intUser_pic && styles.pic_three} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className={styles.interested_count}>
+                    255 others
+                    </div>
+                </div>
             </div>
         </div>    
     );
