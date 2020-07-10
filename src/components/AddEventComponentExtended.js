e228abd87509931ef0e96b1ccae1074b70b933c9
Aman <amankrsingh.2012@gmail.com>
2020-07-10 23:55:01 +0530

    Converted all Components of AddEvent Extended to take input

--- src/components/AddEventComponentExtended.js
+++ src/components/AddEventComponentExtended.js
@@ -4,12 +4,15 @@ import img_Aman from "../temp/image.jpg";
 import PropTypes from "prop-types";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import autosize from "autosize";
+
 // import Test from "./Test";
 
 class CustomInputDatePicker extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <input
@@ -22,65 +25,93 @@ class CustomInputDatePicker extends React.Component {
   }
 }
 
-function AddEventComponentExtended({ onClick }) {
-  const [startDate, setStartDate] = useState(null);
-  const [startTime, setStartTime] = useState(null);
+class AddEventComponentExtended extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      startTime: null,
+    };
+  }
 
-  return (
-    <div className={`${styles.add_event_card} container mb-4`}>
-      <div className="row">
-        <img src={img_Aman} className={styles.add_event_card_image} />
-      </div>
-      <div className="row ml-3 mr-3 mb-2">
-        <div className={`col ${styles.bottom_border_white}`}>
-          <DatePicker
-            showPopperArrow={false}
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            customInput={<CustomInputDatePicker defaultText="Add Date" />}
-          />
+  componentDidMount() {
+    this.textarea.focus();
+    autosize(this.textarea);
+  }
+
+  handleDateChange = (date) => {
+    this.setState({
+      startDate: date,
+    });
+  };
+
+  handleTimeChange = (time) => {
+    this.setState({
+      startTime: time,
+    });
+  };
+
+  render() {
+    return (
+      <div className={`${styles.add_event_card} container mb-4`}>
+        <div className="row">
+          <img src={img_Aman} className={styles.add_event_card_image} />
         </div>
-        <div className="col"></div>
-        <div className={`col ${styles.bottom_border_white}`}>
-          <DatePicker
-            selected={startTime}
-            onChange={(time) => setStartTime(time)}
-            showTimeSelect
-            showTimeSelectOnly
-            timeIntervals={15}
-            timeCaption="Time"
-            dateFormat="h:mm aa"
-            customInput={<CustomInputDatePicker defaultText="Add Time" />}
-          />
+        <div className="row ml-3 mr-3 mb-2">
+          <div className={`col ${styles.bottom_border_white}`}>
+            <DatePicker
+              showPopperArrow={false}
+              selected={this.state.startDate}
+              onChange={this.handleDateChange}
+              customInput={<CustomInputDatePicker defaultText="Add Date" />}
+            />
+          </div>
+          <div className="col"></div>
+          <div className={`col ${styles.bottom_border_white}`}>
+            <DatePicker
+              selected={this.state.startTime}
+              onChange={this.handleTimeChange}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="h:mm aa"
+              customInput={<CustomInputDatePicker defaultText="Add Time" />}
+            />
+          </div>
+        </div>
+        <div className="row ml-3 mr-3 mb-2">
+          {/* Textarea input */}
+          <div className={`col ${styles.bottom_border_white}`}>
+            <textarea
+              placeholder="Write Description"
+              rows={1}
+              ref={(c) => (this.textarea = c)}
+              className={`${styles.cursor_pointer} ${styles.background_inherit} ${styles.placeholder_white} ${styles.font_normal} ${styles.width_inherit} ${styles.border_none} ${styles.font_white}`}
+            />
+          </div>
+        </div>
+        <div className="row ml-3 mr-3 mb-2">
+          <div className={`col ${styles.bottom_border_white}`}>
+            <input
+              placeholder="Paste Class Link"
+              className={`${styles.cursor_pointer} ${styles.background_inherit} ${styles.placeholder_white} ${styles.font_normal} ${styles.width_inherit} ${styles.border_none} ${styles.font_white}`}
+            />
+          </div>
+        </div>
+        <div
+          className={`row ml-3 mr-3 pb-3 mt-3 ${styles.text_centre} ${styles.fw_700}`}
+        >
+          <div
+            className={`col ${styles.cursor_pointer}`}
+            onClick={this.props.onClick}
+          >
+            + ADD EVENT FOR THIS REQUEST
+          </div>
         </div>
       </div>
-      <div className="row ml-3 mr-3 mb-2">
-        {/* Textarea input */}
-        <div className={`col ${styles.bottom_border_white}`}>
-          <textarea
-            placeholder="Write Description"
-            rows={1}
-            className={`${styles.cursor_pointer} ${styles.background_inherit} ${styles.placeholder_white} ${styles.font_normal} ${styles.width_inherit} ${styles.border_none} ${styles.font_white}`}
-          />
-        </div>
-      </div>
-      <div className="row ml-3 mr-3 mb-2">
-        <div className={`col ${styles.bottom_border_white}`}>
-          <input
-            placeholder="Paste Class Link"
-            className={`${styles.cursor_pointer} ${styles.background_inherit} ${styles.placeholder_white} ${styles.font_normal} ${styles.width_inherit} ${styles.border_none} ${styles.font_white}`}
-          />
-        </div>
-      </div>
-      <div
-        className={`row ml-3 mr-3 pb-3 mt-3 ${styles.text_centre} ${styles.fw_700}`}
-      >
-        <div className={`col ${styles.cursor_pointer}`} onClick={onClick}>
-          + ADD EVENT FOR THIS REQUEST
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 CustomInputDatePicker.propTypes = {
