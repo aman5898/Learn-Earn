167e7a8a9b439b03d41ae030247dc11d00a05e6f
bitz1119 <bitz1119@gmail.com>
2020-07-12 22:04:21 +0530

    few changes

--- src/components/UserStamp.js
+++ src/components/UserStamp.js
@@ -9,8 +9,8 @@ function UserStamp() {
   // var classNames = classNames(style.userstamp_bg)
 
   function random_color(){
-    let arr = [["#870000","#190A05"],["#24C6DC","#514A9D"],["#3CA55C","#B5AC49"],["#348F50","#56B4D3"],["#b92b27","#1565C0"],["#f12711","#f5af19"],["#8360c3","#2ebf91"],["#F3904F","#3B4371"],["#3a6186","#89253e"]];
-    return arr[Math.floor(Math.random()*(7))];
+    let arr = [["#870000","#190A05"],["#24C6DC","#514A9D"],["#3CA55C","#B5AC49"],["#348F50","#56B4D3"],["#000046","#1CB5E0"],["#f12711","#f5af19"],["#8360c3","#2ebf91"],["#F3904F","#3B4371"],["#3a6186","#89253e"]];
+    return arr[Math.floor(Math.random()*(arr.length))];
   }
 
   let colors = random_color();
--- src/components/YourRequests.js
+++ src/components/YourRequests.js
@@ -3,6 +3,8 @@ import {Card} from 'react-bootstrap';
 import styles from "../styles/App.scss";
 import UserStamp from './UserStamp'
 import classNames from 'classnames'
+import { faHandshake } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 function YourRequests(){
@@ -14,12 +16,12 @@ function YourRequests(){
     }
     const maincardcns = classNames(styles.yourrequest_main_card,styles.card)
     const cns = classNames(styles.font_color_70,styles.font_size_9,styles.yourrequest_inner_card)
-    
+    const cns_title = classNames(styles.card_title , styles.yourrequest_title)
     return(
         <Card className = {maincardcns}>
             <Card.Body>
-            <Card.Title className={styles.card_title && styles.yourrequest_title} >
-                Your Requests
+            <Card.Title className={cns_title} >
+                <FontAwesomeIcon icon={faHandshake}/> Your Requests
                 </Card.Title>
             <Card className={styles.hidden_border}>
                 <UserStamp />
