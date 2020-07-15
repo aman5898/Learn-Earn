6f5e6e2c29d107e6c2293cd63c8a765d90b0e208
Aman <amankrsingh.2012@gmail.com>
2020-07-15 14:06:19 +0530

    removed redux Provider from index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
-import configureStore from "./redux/configureStore";
-import { Provider as ReduxProvider } from "react-redux";
-
-const store = configureStore();
 
 render(
-  <ReduxProvider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </ReduxProvider>,
+  <Router>
+    <App />
+  </Router>,
+
   document.getElementById("app")
 );
